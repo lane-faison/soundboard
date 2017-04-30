1e5bf3268d2f8fcb62729dc4ae10be52f78ae577
Lane Faison <lane.faison@icloud.com>
2017-04-30 09:25:59 -0600

    Buttons added and linked

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Lane Faison on 4/30/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+   
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    
+    }
+    
+}
